9bd399213600c3f9c7832780e6a443c5b385c1f3
potatoqualitee <clemaire@gmail.com>
2018-03-28 17:56:38 +0200

    prep

--- tests/Invoke-DbaLogShipping.Tests.ps1
+++ tests/Invoke-DbaLogShipping.Tests.ps1
@@ -3,13 +3,18 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-    # This is a placeholder until we decide on sql2016/sql2017
     BeforeAll {
         $dbname = "dbatoolsci_logshipping"
+        Get-DbaProcess -SqlInstance $script:instance2 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $server.Query("CREATE DATABASE $dbname; ALTER DATABASE $dbname SET AUTO_CLOSE OFF WITH ROLLBACK IMMEDIATE")
     }
-
-    It -Skip "returns success" {
-        $results = Invoke-DbaLogShipping -SourceSqlInstance $script:instance2 -DestinationSqlInstance $script:instance -Database $dbname -BackupNetworkPath C:\temp -BackupLocalPath "C:\temp\logshipping\backup" -GenerateFullBackup -CompressBackup -SecondaryDatabaseSuffix "_LS" -Force
+    AfterAll {
+        Get-DbaDatabase -SqlInstance $script:instance2, $script:instance3 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    }
+    
+    It -Skip "sets up log shipping properly" {
+        $results = Invoke-DbaLogShipping -SourceSqlInstance $script:instance2 -DestinationSqlInstance $script:instance3 -Database $dbname -BackupNetworkPath C:\temp -BackupLocalPath "C:\temp\logshipping\backup" -GenerateFullBackup -CompressBackup -SecondaryDatabaseSuffix "_LS" -Force
         $results.Status -eq 'Success' | Should Be $true
     }
 }
